c3585f37851c8c58aa48bd5a512b1bed04883cba
saniyasaher20 <saniyasaher20@gmail.com>
2025-05-30 20:09:35 +0530

    partials made

--- resources/views/pdf/proposal.blade.php
+++ resources/views/pdf/proposal.blade.php
@@ -311,6 +311,20 @@
 			line-height: 1;
 			letter-spacing: -0.2;
 		}
+
+		.p-0 {
+			padding: 0;
+		}
+
+		.m-0 {
+			margin: 0;
+		}
+
+		.table-no-spacing th,
+		.table-no-spacing td {
+			padding: 0;
+			margin: 0;
+		}
 	</style>
 
 </head>
@@ -405,133 +419,7 @@
 	</section> --}}
 
 	{{-- SINGLE ITEM PAGE --}}
-	<section class="a4-size page-break">
-		<table class="equal-width table">
-			<tr>
-				<td><span class="fw-bold text-sm">PROJECT:</span> <br><span class="fw-bold text-md">SAROVAR PORTICO, MUMBAI</span>
-				</td>
-				<td><span class="fw-bold text-sm">MANUFACTURER'S DETAILS:</span> <br><span class="fw-bold text-md">To bid</span></td>
-				<td class="fw-bold text-md" style="text-align: center !important;">ITEM CODE:<br><span class="text-md">FF -
-						301</span>
-				</td>
-			</tr>
-			<tr>
-				<td><span class="fw-bold text-sm">ITEM NAME:</span> <br><span class="text-md">Dining table</span></td>
-				<td><span class="fw-bold text-sm">DESCRIPTION:</span> <br><span class="text-md">Laminate with metal and tile top
-						dining table</span></td>
-				<td><span class="fw-bold text-sm">TOTAL QUANTITY:</span> <br><span class="text-md">22 nos</span></td>
-			</tr>
-			<tr>
-				<td><span class="fw-bold text-sm">AREA/LOCATION:</span> <br><span class="text-md">All day dining</span></td>
-				<td><span class="fw-bold text-sm">PRODUCT CODE:</span> <br><span class="text-md"></span></td>
-				<td><span class="fw-bold text-sm">REVISION:</span> <br><span class="text-md"></span></td>
-			</tr>
-			<tr>
-				<td class="fw-bold uppercase" style="border-bottom:0;" colspan="3">SKETCH / PHOTOS / MATERIAL / SWATCH REFERENCE
-					/
-					SPECIFICATIONS / NOTES</td>
-			</tr>
-			<tr>
-				<td style="border-top:0;" colspan="3">
-					<table class="table-borderless" style="width: 100%; margin:0;padding:0;">
-						{{-- INFO AND THUMBNAIL --}}
-						<tr>
-							<!-- LEFT: TEXT INFO -->
-							<td style="width: 50%; vertical-align: top; padding-right: 10px;" colspan="3">
-								Model number: Custom furniture<br>
-								Model name: Custom made furniture<br>
-								Width: 700 mm<br>
-								Length: 900 mm<br>
-								Height: 750 mm<br>
-								Material: Metal, wood and tile<br>
-								Details: Refer to FF&E shop drawing<br>
-								Note: Supplier to coordinate the items structure and stability.
-							</td>
 
-							<!-- RIGHT: IMAGE -->
-							<td style="width: 40%; vertical-align: top; text-align: right;" rowspan="2">
-								<img src="{{ public_path('images/item thumbnail 1.png') }}" alt="Furniture"
-									style="max-height: 100%; max-width: 100%; width: 100%; object-fit: contain; border: 1px solid #ccc; padding: 2px; display: block; margin-left: auto;" />
-							</td>
-						</tr>
-
-						{{-- SWATCHES --}}
-						<tr>
-							<td style="text-align: left;" colspan="3">
-								<div class="img-sm-wrapper">
-									<div class="img-sm"
-										style="
-									background: url('{{ public_path('images/swatches (2).png') }}') center center/cover no-repeat;">
-									</div>
-									<div style="font-size: 12px; margin-top: 1px;">TILE FINISH <br> TL- 02</div>
-								</div>
-								<div class="spacer"></div> <!-- spacer -->
-								<div class="img-sm-wrapper">
-									<div class="img-sm"
-										style="
-									background: url('{{ public_path('images/swatches (2).png') }}') center center/cover no-repeat;">
-									</div>
-									<div style="font-size: 12px; margin-top: 1px;">TILE FINISH <br> TL- 02</div>
-
-								</div>
-								<div class="spacer"></div> <!-- spacer -->
-								<div class="img-sm-wrapper">
-									<div class="img-sm"
-										style="
-									background: url('{{ public_path('images/swatches (2).png') }}') center center/cover no-repeat;">
-									</div>
-									<div style="font-size: 12px; margin-top: 1px;">TILE FINISH <br> TL- 02</div>
-								</div>
-							</td>
-						</tr>
-
-						{{-- GENERAL NOTES --}}
-						<tr>
-							<td class="text-sm text-condensed" colspan="4">
-								<div class="fw-bold uppercase">GENERAL NOTES: </div>
-								<div>* Manufacturer to ensure support and stability of product.</div>
-								<div>* All construction methods and finishes must be of contract quality suitable for commercial and hospitality
-									use, of new high-grade material, clean and free from defects in material and workmanship/fabrication in
-									accordance
-									with House of Elm.</div>
-								<div>* All Local Codes for installation methods and practices must be observed.</div>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
-		</table>
-		{{-- CONTACT --}}
-		<table class="table">
-			<tr>
-				<td class="fw-bold text-sm uppercase" style="width: 60%; border-top:none;">
-					<div>CONTACT PERSON:</div>
-					<div>TELEPHONE:</div>
-					<div>FACSIMILE:</div>
-					<div>E-MAIL:</div>
-					<div>WEBSITE</div>
-				</td>
-				<td style="border-top:none;">
-					<div class="fw-bold text-sm uppercase">REQUIRED ITEMS FOR DESIGN APPROVAL PRIOR TO FABRICATION:</div>
-					<ul class="text-sm" style="line-height: 1;margin-left:-20px;margin-top: 0px;">
-						<li>Shop Drawing</li>
-						<li>Finish Sample</li>
-						<li>Prototype</li>
-					</ul>
-				</td>
-			</tr>
-			<tr>
-				<td class="text-xs text-condensed"  colspan="2">
-					<div class="fw-bold uppercase">REMARKS: </div>
-					This is a preliminary information sheet only and not for construction purposes. Refer to material reference boards
-					& fabric swatch submitted by STUDIO for actual samples. Manufacturer to provide STUDIO with shop drawings and
-					prototype sample pieces for approval prior to production. Manufacturer to work in conjunction with designer to
-					ensure proper proportions & design intent. The furniture, fittings & equipment pieces in this specification book is
-					for design intent only.
-				</td>
-			</tr>
-		</table>
-	</section>
 
 	{{-- DRAWING PAGE --}}
 	<section class="a4-size page-break" style="outline: 1px solid black; position: relative;">
@@ -613,29 +501,29 @@
 						<div class="text-condensed text-xs"> ADDRESS:</div> <b>MUMBAI, INDIA</b> <br><br>
 					</td>
 					<td>
-						<table class="table-borderless text-xs text-condensed" cellspacing="0" cellpadding="0" >
+						<table class="text-condensed table-no-spacing text-xs" cellspacing="0" cellpadding="0">
 							<tr>
 								<td><b>PROJECT #:</b></td>
 								<td><b>20031</b></td>
 							</tr>
 							<tr>
-								<td><b>Issue date:</b></td>
+								<td>Issue date:</td>
 								<td><!-- Add issue date here --></td>
 							</tr>
 							<tr>
-								<td><b>Studio principal:</b></td>
+								<td>Studio principal:</td>
 								<td>Milouni Mehta</td>
 							</tr>
 							<tr>
-								<td><b>Drawn by:</b></td>
+								<td>Drawn by:</td>
 								<td>Mahavira Gupta</td>
 							</tr>
 							<tr>
-								<td><b>Checked by:</b></td>
+								<td>Checked by:</td>
 								<td>Milouni Mehta</td>
 							</tr>
 							<tr>
-								<td><b>Scale:</b></td>
+								<td>Scale:</td>
 								<td>As shown</td>
 							</tr>
 						</table>
